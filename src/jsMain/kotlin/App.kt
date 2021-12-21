65a867d44bce3bb18c481bb13beb02504b643c03
JC Francisco <jc.francisco17@gmail.com>
2021-12-21 17:06:15 +0800

    Frontend

---
+++ src/jsMain/kotlin/App.kt
@@ -0,0 +1,53 @@
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.html.js.onClickFunction
+import react.Props
+import react.dom.h1
+import react.dom.ul
+import react.dom.li
+import react.fc
+import react.useEffectOnce
+import react.useState
+
+private val scope = MainScope()
+
+val app = fc<Props> {
+
+    var shoppingList by useState(emptyList<ShoppingListItem>())
+
+    useEffectOnce {
+        scope.launch {
+            shoppingList = getShoppingList()
+        }
+    }
+
+    h1 {
+        +"Full-Stack Shopping List"
+    }
+
+    ul {
+        shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+            li {
+                key = item.toString()
+                attrs.onClickFunction = {
+                    scope.launch {
+                        deleteShoppingListItem(item)
+                        shoppingList = getShoppingList()
+                    }
+                }
+                +"[${item.priority}] ${item.desc}"
+            }
+        }
+    }
+
+    child(inputComponent) {
+        attrs.onSubmit = { input ->
+            val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
+            scope.launch {
+                addShoppingListItem(cartItem)
+                shoppingList = getShoppingList()
+            }
+        }
+    }
+
+}
